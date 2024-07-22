29e3b9af79f6bb8244d8915d7783559740c0024d
Kheyda <khanyamelapi@gmail.com>
2024-07-22 16:00:17 +0200

    changes

--- src/Pages/Home/MySkills.jsx
+++ src/Pages/Home/MySkills.jsx
@@ -1,6 +1,14 @@
 import data from "../../data/index.json";
 
 export default function MySkills() {
+  const clickableImages = [
+    { src: "./img/makasana.png", link: "https://makasana.vercel.app" },
+    { src: "./img/tech.png", link: "https://splendorous-wisp-9628e5.netlify.app/" },
+    { src: "./img/land.png", link: "https://candid-marigold-9c7448.netlify.app" },
+    { src: "./img/sneak.png", link: "https://deluxe-khapse-e89819.netlify.app" },
+    { src: "./img/chat.png", link: "" },
+  ];
+
   return (
     <section className="skills--section" id="mySkills">
       <div className="portfolio--container">
@@ -22,6 +30,14 @@ export default function MySkills() {
               </div>
             </div>
           </div>
+        ))}
+      </div>
+      <h2 className="view-projects--heading">View My Projects</h2>
+      <div className="clickable-images--grid">
+        {clickableImages.map((image, index) => (
+          <a key={index} href={image.link} target="_blank" rel="noopener noreferrer">
+            <img src={image.src} alt={`Clickable ${index + 1}`} className="clickable-image" />
+          </a>
         ))}
       </div>
     </section>
